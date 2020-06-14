1d9fe216c6045ca353890bda7ebff395549cbb39
Fvanbrummelen <darthfebo@users.noreply.github.com>
2020-06-14 18:05:36 +0200

    First commit

---
+++ Game/Assets/script/enemyScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enemyScript : MonoBehaviour
+{
+    private Transform player;
+    private float dis;
+    public float moveSpeed;
+    public float enemyRadius;
+
+     void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+
+     void Update()
+    {
+        dis = Vector3.Distance(player.position, transform.position);
+        if(dis <= enemyRadius)
+        {
+            transform.LookAt(player);
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed);
+        }
+
+
+        
+
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, enemyRadius);
+    }
+}
+
+
---
+++ Game/Assets/script/oldPlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 600f;
+    public float backForce = 520f;
+    public float sideForce = 550f;
+    public float jumpForce = 1000f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!"); 
+    }
+
+    // Update is called once per frame
+    // when you work with physics unity likes it better if you use FixedUpdate
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey("d")) 
+        { rb.AddForce(sideForce * Time.deltaTime, 0, 0); }
+        if (Input.GetKey("a"))
+        { rb.AddForce(sideForce * -1 * Time.deltaTime, 0, 0); }
+        if (Input.GetKey("w"))
+        { rb.AddForce(0, 0, forwardForce * Time.deltaTime); }
+        if (Input.GetKey("s"))
+        { rb.AddForce(0, 0, backForce * Time.deltaTime * -1); }
+        if (Input.GetKey("space"))
+        { rb.AddForce(0, jumpForce*Time.deltaTime, 0); }
+    }
+}
+
